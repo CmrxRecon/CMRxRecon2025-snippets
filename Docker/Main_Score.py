e2502afbf4b12930cd94a3dd00b9b4df52a7501a
许自强 <ziqiang_xu@qq.com>
2023-06-08 15:24:48 +0000

    fix: replace nan with -1

--- Docker/Main_Score.py
+++ Docker/Main_Score.py
@@ -703,6 +703,10 @@ def main():
             "Mapping_NMSE": nmse_mean
         }
     with open(args.results, "w") as out:
+        for k, v in scores.items():
+            print(type(v), v)
+            if np.isnan(v):
+                scores[k] = -1
         results = {
             "submission_status": "SCORED",
             **scores
